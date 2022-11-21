68a5d93ec37e69038caa198782189b1221127b83
maoz.h <pongpat.phokeed@gmail.com>
2022-11-21 10:56:53 +0700

    add method index

--- src/main/java/com/example/demo2/controller/AppRestController.java
+++ src/main/java/com/example/demo2/controller/AppRestController.java
@@ -1,4 +1,15 @@
 package com.example.demo2.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AppRestController {
+
+    @GetMapping("/xxxx")
+    public String getIndex(){
+        return "xxx";
+    }
+
 }
